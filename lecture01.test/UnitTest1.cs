df9417eed85eb5c9f39227f0fa857d8b826efe0b
Gustavholm2 <49881816+Gustavholm2@users.noreply.github.com>
2021-09-03 14:10:45 +0200

    boiler plate / project setup (#1)
    * created sln file
    
    * did everything so far
    
    Co-authored-by: Gustav Samuel Bonde Holmgren <guho@itu.dk>

---
+++ lecture01.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace lecture01.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void prints_out_hello_world()
+        {
+            
+        }
+    }
+}

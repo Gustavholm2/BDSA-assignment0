e1730a72191421b2b26d3275006d657e8ec3fd6a
Gustavholm2 <49881816+Gustavholm2@users.noreply.github.com>
2021-09-06 13:37:20 +0200

    Implemented leap year function (#3)
    * created sln file
    
    * did everything so far
    
    * Setup method signature and test for divisible by 4
    
    * made divisble by 100 test
    
    * made a test for divisible by four hundred
    
    * finished the IsLeapYear function
    
    Co-authored-by: Gustav Samuel Bonde Holmgren <guho@itu.dk>

--- lecture01.test/UnitTest1.cs
+++ lecture01.test/UnitTest1.cs
@@ -6,9 +6,27 @@ namespace lecture01.test
     public class UnitTest1
     {
         [Fact]
-        public void prints_out_hello_world()
+        public void not_leap_year() 
         {
-            
+            Assert.False(Program.IsLeapYear(5));
+        }
+
+        [Fact]
+        public void divisible_by_four()
+        {
+            Assert.True(Program.IsLeapYear(16));
+        }
+
+        [Fact]
+        public void divisible_by_one_hundred() 
+        {
+            Assert.False(Program.IsLeapYear(1900));
+        }
+
+        [Fact]
+        public void divisible_by_four_hundred() 
+        {
+            Assert.True(Program.IsLeapYear(2000));
         }
     }
 }
--- lecture01/Program.cs
+++ lecture01/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+using System;
 
 namespace lecture01
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsLeapYear(int year) 
+        {
+            if (year % 400 == 0) {
+                return true;
+            }
+            if (year % 100 == 0) {
+                return false;
+            }
+            if (year % 4 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }

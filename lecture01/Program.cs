2d9a998aecef9bf0353bf1542b8d062fab76261a
Gustav Samuel Bonde Holmgren <guho@itu.dk>
2021-09-06 14:45:46 +0200

    implemented user input function

--- lecture01/Program.cs
+++ lecture01/Program.cs
@@ -6,7 +6,9 @@ namespace lecture01
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("write a year and hit [ENTER] >");
+            int.TryParse(Console.ReadLine(), out int input);
+            Console.WriteLine(IsLeapYear(input)? "yay" : "nay");
         }
 
         public static bool IsLeapYear(int year) 

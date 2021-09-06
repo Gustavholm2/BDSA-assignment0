04d0530dc2398282e28ead4ccbd92f3a595a0a04
Gustav Samuel Bonde Holmgren <guho@itu.dk>
2021-09-06 14:43:17 +0200

    made test for user input

--- lecture01.test/UnitTest1.cs
+++ lecture01.test/UnitTest1.cs
@@ -1,9 +1,10 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace lecture01.test
 {
-    public class UnitTest1
+    public class IsLeapYearTest
     {
         [Fact]
         public void not_leap_year() 
@@ -29,4 +30,41 @@ namespace lecture01.test
             Assert.True(Program.IsLeapYear(2000));
         }
     }
+
+    public class MainTest
+    {
+        [Fact]
+        public void Yay()
+        {
+        //Arrange
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        //Act
+        Console.SetIn(new StringReader("2020"));
+        Program.Main(new String[0]);
+        var output = writer.GetStringBuilder().ToString().Trim()[^3..^0];
+        
+        //Assert
+        Assert.Equal("yay", output);
+
+        }
+
+        [Fact]
+        public void Nay()
+        {
+        //Arrange
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        
+        //Act
+        Console.SetIn(new StringReader("2021"));
+        Program.Main(new String[0]);
+        var output = writer.GetStringBuilder().ToString().Trim()[^3..^0];
+        
+        //Assert
+        Assert.Equal("nay", output);
+        }
+    }
+
 }
--- lecture01/Program.cs
+++ lecture01/Program.cs
@@ -4,9 +4,9 @@ namespace lecture01
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
 
         public static bool IsLeapYear(int year) 

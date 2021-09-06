3ee6856fd15f1dd9aff3655a31de369032799f8a
Gustav Samuel Bonde Holmgren <guho@itu.dk>
2021-09-06 13:30:24 +0200

    finished the IsLeapYear function

--- lecture01/Program.cs
+++ lecture01/Program.cs
@@ -11,6 +11,9 @@ namespace lecture01
 
         public static bool IsLeapYear(int year) 
         {
+            if (year % 400 == 0) {
+                return true;
+            }
             if (year % 100 == 0) {
                 return false;
             }
